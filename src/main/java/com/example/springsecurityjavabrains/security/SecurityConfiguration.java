b5a170eba2af35e235640d62fdb0ebd53ea3750f
Eran37 <doc.assraf@gmail.com>
2023-01-26 14:32:52 +0200

    Initial commit

---
+++ src/main/java/com/example/springsecurityjavabrains/security/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.springsecurityjavabrains.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //Set your config on the auth obj.
+        auth.inMemoryAuthentication()
+                .withUser("bla")
+                .password("bla")
+                .roles("USER")
+                .and()
+                .withUser("bli")
+                .password("bli")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+}
